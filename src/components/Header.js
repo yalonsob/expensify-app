3cc82b735499cabbc236f8e8f84295fda7a510d6
yalonsob <yalonsob@gmail.com>
2018-03-16 19:47:06 -0700

    Setup production webpack build

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
     <header>
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>u
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
     </header>
 );
 
