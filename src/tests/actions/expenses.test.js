12cc6d1ce7f30b6719c9bc2d7266ca3d9d578940
yalonsob <yalonsob@gmail.com>
2018-03-31 17:20:07 -0700

    Add startEditExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -3,11 +3,12 @@ import thunk from 'redux-thunk';
 import { 
     startAddExpense, 
     addExpense, 
-    editExpense, 
-    removeExpense, 
+    editExpense,
+    startEditExpense, 
+    removeExpense,
+    startRemoveExpense, 
     setExpenses, 
-    startSetExpenses, 
-    startRemoveExpense 
+    startSetExpenses 
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
@@ -54,6 +55,24 @@ test('should setup edit expense action object', () => {
         updates: {
             note: 'New note value'
         }
+    });
+});
+
+test('should edit expense from firebase', (done) => {
+    const store = createMockStore();
+    const id = expenses[0].id;
+    const updates = { amount: 21045 };
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        });
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val().amount).toBe(updates.amount);
+        done();
     });
 });
 
